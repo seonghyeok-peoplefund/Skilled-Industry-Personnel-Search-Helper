d6219033ec416c1b58eb623d1873e94da31414b3
Ray.Jang <ray.jang@moneybrain.ai>
2021-04-02 14:19:18 +0900

    Spinner 업데이트

---
+++ app/src/main/java/com/ray/personnel/Company/CompanyOccupation.kt
@@ -0,0 +1,131 @@
+package com.ray.personnel.Company
+
+import java.util.*
+
+class CompanyOccupation {
+    companion object{
+        val occupation :Map<String, Map<String, Int>> =
+                mapOf(
+                        "서버" to mapOf("서버 개발자" to 872,
+                                "웹 개발자" to 873,
+                                "프론트엔드 개발자" to 669,
+                                "자바 개발자" to 660,
+                                "안드로이드 개발자" to 677,
+                                "소프트웨어 엔지니어" to 10110,
+                                "iOS 개발자" to 678,
+                                "파이썬 개발자" to 899,
+                                "데이터 엔지니어" to 655,
+                                "Node.js 개발자" to 895,
+                                "DevOps / 시스템 관리자" to 674,
+                                "머신러닝 엔지니어" to 1634,
+                                "시스템,네트워크 관리자" to 665,
+                                "C,C++ 개발자" to 900,
+                                "데이터 사이언티스트" to 1024,
+                                "빅데이터 엔지니어" to 1025,
+                                "개발 매니저" to 877,
+                                "QA,테스트 엔지니어" to 676,
+                                "PHP 개발자" to 893,
+                                "기술지원" to 1026,
+                                "프로덕트 매니저" to 876,
+                                "보안 엔지니어" to 671,
+                                "웹 퍼블리셔" to 939,
+                                "하드웨어 엔지니어" to 672,
+                                ".NET 개발자" to 661,
+                                "블록체인 플랫폼 엔지니어" to 1027,
+                                "임베디드 개발자" to 658,
+                                "영상,음성 엔지니어" to 896,
+                                "크로스플랫폼 앱 개발자" to 10111,
+                                "루비온레일즈 개발자" to 894,
+                                "BI 엔지니어" to 1022,
+                                "CTO,Chief Technology Officer" to 795,
+                                "그래픽스 엔지니어" to 898,
+                                "VR 엔지니어" to 10112,
+                                "CIO,Chief Information Officer" to 793),
+                        "경영/비즈니스" to mapOf("서비스 기획자" to 565,
+                                "사업개발·기획자" to 564,
+                                "PM·PO" to 559,
+                                "전략 기획자" to 563,
+                                "운영 매니저" to 554,
+                                "경영지원" to 552,
+                                "데이터 분석가" to 656,
+                                "회계·경리" to 1034,
+                                "컨설턴트" to 570,
+                                "총무" to 562,
+                                "해외 사업개발·기획자" to 10115,
+                                "자금담당" to 10116,
+                                "조직관리" to 567,
+                                "오피스 관리" to 557,
+                                "리스크 관리 전문가" to 569,
+                                "경영 혁신가" to 568,
+                                "사무보조" to 10122,
+                                "지역 관리 매니저" to 558,
+                                "구매담당" to 561,
+                                "COO,Chief Operation Officer" to 794,
+                                "CFO,Chief Financial Officer" to 792,
+                                "세미나/포럼 기획자" to 10118,
+                                "비서" to 550,
+                                "CSO,Chief Strategy Officer" to 798,
+                                "전시 기획자" to 10117,
+                                "애자일코치" to 10120,
+                                "지점장" to 560,
+                                "CEO,Chief Executive Officer" to 791,
+                                "안내원" to 555,
+                                "사내 심리상담가" to 10121,
+                                "공연 기획자" to 10119),
+                        "마케팅/광고" to mapOf(
+
+                        ),
+                        "디자인" to mapOf(
+
+                        ),
+                        "영ㅇㅂ" to mapOf(
+
+                        ),
+                        "고객서비스/리테일" to mapOf(
+
+                        ),
+                        "미디어" to mapOf(
+
+                        ),
+                        "인사" to mapOf(
+
+                        ),
+                        "게임 제작" to mapOf(
+
+                        ),
+                        "금융" to mapOf(
+
+                        ),
+                        "물류/무역" to mapOf(
+
+                        ),
+                        "엔지니어링/설계" to mapOf(
+
+                        ),
+                        "의료/제약/바이오" to mapOf(
+
+                        ),
+                        "제조/생산" to mapOf(
+
+                        ),
+                        "교육" to mapOf(
+
+                        ),
+                        "식/음료" to mapOf(
+
+                        ),
+                        "법률/법집행기관" to mapOf(
+
+                        ),
+                        "건설/시설" to mapOf(
+
+                        ),
+                        "공공/복지" to mapOf(
+
+                        )
+
+
+                )
+
+    }
+}
